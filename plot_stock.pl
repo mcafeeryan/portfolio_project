271d1c4e18c595bbc9e69c1d78de957101420380
msh986 <matthewhlavacek2014@u.northwestern.edu>
2013-02-01 12:30:15 -0600

    big update here

--- plot_stock.pl
+++ plot_stock.pl
@@ -5,6 +5,9 @@ use strict;
 use CGI qw(:standard);
 use DBI;
 use Time::ParseDate;
+use Time::CTime;
+use Date::Manip;
+use Time::Local;
 
 BEGIN {
   $ENV{PORTF_DBMS}="oracle";
@@ -27,6 +30,14 @@ use stock_data_access;
 
 my $type = param('type');
 my $symbol = param('symbol');
+my $start_timestamp=param('start_timestamp');
+my $end_timestamp=param('end_timestamp');
+my $start_day=param('start_day');
+my $start_month=param('start_month');
+my $start_year=param('start_year');
+my $end_day=param('end_day');
+my $end_month=param('end_month');
+my $end_year=param('end_year');
 
 if (!defined($type) || $type eq "text" || !($type eq "plot") ) { 
   print header(-type => 'text/html', -expires => '-1h' );
@@ -53,9 +64,16 @@ if (!defined($type) || $type eq "text" || !($type eq "plot") ) {
   }
 }
 
-
-my @rows = ExecStockSQL("2D","select timestamp, close from ".GetStockPrefix()."StocksDaily where symbol=rpad(?,16)",$symbol);
-
+if((!defined($start_timestamp)||!defined($end_timestamp)) && defined($start_day) && defined($start_month)  && defined($start_year) && defined($end_day) && defined($end_month)  && defined($end_year))
+{    $start_timestamp = timelocal( 0, 0, 0, $start_day, $start_month, $start_year );
+     $end_timestamp   = timelocal( 0, 0, 0, $end_day,   $end_month,   $end_year );
+}
+elsif(!defined($start_timestamp)||!defined($end_timestamp)){
+     $start_timestamp = 0;
+     $end_timestamp   = time();
+   }
+my @rows = ExecStockSQL("2D","(select (TO_DATE('19700101000000','YYYYMMDDHH24MISS')+ NUMTODSINTERVAL(timestamp, 'SECOND')) as DateOfQuote, close from cs339.StocksDaily where symbol=rpad(?,16) and timestamp>? and timestamp<? union all select (TO_DATE('19700101000000','YYYYMMDDHH24MISS')+ NUMTODSINTERVAL(timestamp, 'SECOND')) as DateOfQuote, close from rpm267.new_stocks_daily where symbol=rpad(?,16) and timestamp>? and timestamp<?) order by DateOfQuote",$symbol,$start_timestamp,$end_timestamp,$symbol,$start_timestamp,$end_timestamp);
+#my @rows = ExecStockSQL("2D","select timestamp, close from cs339.StocksDaily where symbol=rpad(?,16) order by timestamp union all select (TO_DATE('19700101000000','YYYYMMDDHH24MISS')+ NUMTODSINTERVAL(timestamp, 'SECOND')) as DateOfQuote, close from rpm267.new_stocks_daily where symbol=rpad(?,16)) order by DateOfQuote",$symbol,$symbol);
 if ($type eq "text") { 
   print "<pre>";
   foreach my $r (@rows) {
@@ -74,10 +92,14 @@ if ($type eq "text") {
 # at stdin, and it will print the graph for us to stdout
 #
 #
+
   open(GNUPLOT,"| gnuplot") or die "Cannot run gnuplot";
   
   print GNUPLOT "set term png\n";           # we want it to produce a PNG
   print GNUPLOT "set output\n";             # output the PNG to stdout
+  print GNUPLOT "set xdata time\n";
+  print GNUPLOT "set timefmt '%d-%b-%y'\n";
+  print GNUPLOT "set format x '%m/%y'\n";
   print GNUPLOT "plot '-' using 1:2 with linespoints\n"; # feed it data to plot
   foreach my $r (@rows) {
     print GNUPLOT $r->[0], "\t", $r->[1], "\n";
